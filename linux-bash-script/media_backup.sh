a196e2bf78ff30465850e893dc878ab58bdbea17
Ayodele Seriki <serikiayodele@gmail.com>
2023-01-25 18:28:43 +0100

    linux bash script

--- linux-bash-script/media_backup.sh
+++ linux-bash-script/media_backup.sh
@@ -1,12 +1,15 @@
 #!/bin/bash
 
-zip -r xfusioncorp_blog.zip /var/www/html/blog
+zip -r xfusioncorp_media.zip /var/www/html/media
 
 # Save the archive to the temporary backup location
-cp xfusioncorp_blog.zip /backup/
+cp xfusioncorp_media.zip /backup/
 
 # Save the archive to the Nautilus Backup Server
-scp xfusioncorp_blog.zip clint@stbkp01:/backup
+scp xfusioncorp_media.zip clint@stbkp01:/backup
 
 # Copy the archive to the Nautilus Backup Server
-scp xfusioncorp_blog.zip clint@stbkp01:/backup
+scp xfusioncorp_media.zip clint@stbkp01:/backup
+
+
+
